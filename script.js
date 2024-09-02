3e08490f22898be4d169c9fb5652d419b64565a3
CookieIDHRL <92392647+CookieIDHRL@users.noreply.github.com>
2024-09-03 00:04:07 +0100

    Fix filter

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function filterTable() {
-    var input, filter, table, tr, td1, td2, i, txtValue1, txtValue2;
-    input = document.getElementById("inputBox");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("commandTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td1 = tr[i].getElementsByTagName("td")[0];
-        td2 = tr[i].getElementsByTagName("td")[1];
-        if (td1) {
-            txtValue1 = td1.textContent || td1.innerText;
-            if (txtValue1.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else if (td2) {
-                txtValue2 = td2.textContent || td2.innerText;
-                if (txtValue2.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
+    let input = document.getElementById("inputBox"),
+        filter = input.value.toUpperCase(),
+        table = document.getElementById("mainTable"),
+        tableRows = table.getElementsByTagName("tr");
+    for (let i = 0; i < tableRows.length; i++) {
+        let rowData = tableRows[i].getElementsByTagName("td"),
+            showBool = false;
+        for (let j = 0; j < rowData.length; j++) {
+            let cellData = rowData[j];
+            if (cellData) {
+                let txtValue = cellData.textContent || cellData.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    showBool = true;
+                    break;
                 }
             }
+        }
+        if (showBool) {
+            tableRows[i].style.display = "";
+        } else {
+            tableRows[i].style.display = "none";
         }
     }
 }
